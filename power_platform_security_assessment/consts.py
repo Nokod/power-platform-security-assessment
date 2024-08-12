9df01373907a54bd083524248ab9546272aff5bb
Nokod Security <dev@nokodsecurity.com>
2024-08-12 15:42:38 +0300

    Fetch environments

---
+++ power_platform_security_assessment/consts.py
@@ -0,0 +1,7 @@
+class Requests:
+    AUTHORITY = "https://login.microsoftonline.com/common"
+    CLIENT_ID = "23d8f6bd-1eb0-4cc2-a08c-7bf525c67bcd"
+    SCOPE = ['https://service.powerapps.com/.default']
+
+class ResponseKeys:
+    ACCESS_TOKEN = "access_token"
