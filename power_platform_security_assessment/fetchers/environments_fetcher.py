82fe3313c5e522a48670899505f9de20bdfeb307
Tomer <tomer@nokodsecurity.com>
2024-09-08 16:53:15 +0300

    update cmd

--- power_platform_security_assessment/fetchers/environments_fetcher.py
+++ power_platform_security_assessment/fetchers/environments_fetcher.py
@@ -20,11 +20,11 @@ class EnvironmentsFetcher:
         print(f'Total number of environments: {len(environments)}')
         print()
         print(
-            f'{"ID":<44} {"Name":<{max_display_name_length}} {"Created By":<20} {"Create Time":<30} {"Last Activity":<30} {"Type":<10} {"State":<10}')
+            f'{"ID":<44} {"Name":<{max_display_name_length}} {"Created By":<20} {"Create Time":<30} {"Last Activity":<30} {"Type":<10}')
         for env in environments:
             created_by = env.properties.createdBy.get('displayName', 'N/A')
             print(
-                f'{env.id.split("/")[-1]:<44} {env.properties.displayName:<{max_display_name_length}} {created_by:<20} {env.properties.createdTime:<30} {env.properties.lastActivity.lastActivity.lastActivityTime:<30} {env.properties.environmentSku:<10} {env.properties.provisioningState:<10}')
+                f'{env.id.split("/")[-1]:<44} {env.properties.displayName:<{max_display_name_length}} {created_by:<20} {env.properties.createdTime:<30} {env.properties.lastActivity.lastActivity.lastActivityTime:<30} {env.properties.environmentSku:<10}')
         print()
 
     def _notify_user(self, total_envs: int):
